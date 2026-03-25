a6c09f108fceb269a0431e416bf096924a6edc7c
Kozlowski, Alexandre (Stud. DACS) <a.kozlowski@student.maastrichtuniversity.nl>
2026-03-25 18:09:31 +0100

    added gitignore for the tarfer

--- target/debug/deps/cargo_compiler-fe3fae3c46b601ae.d
+++
@@ -1,5 +0,0 @@
-/Users/alexandre/Documents/computer architecture/c code/quack/quompiler/target/debug/deps/cargo_compiler-fe3fae3c46b601ae.d: compiler/cargo_compiler/src/main.rs
-
-/Users/alexandre/Documents/computer architecture/c code/quack/quompiler/target/debug/deps/libcargo_compiler-fe3fae3c46b601ae.rmeta: compiler/cargo_compiler/src/main.rs
-
-compiler/cargo_compiler/src/main.rs:
--- target/debug/deps/rust_assembler-02d2140120425918.d
+++
@@ -1,5 +0,0 @@
-/Users/alexandre/Documents/computer architecture/c code/quack/quompiler/target/debug/deps/rust_assembler-02d2140120425918.d: assembler/rust_assembler/src/main.rs
-
-/Users/alexandre/Documents/computer architecture/c code/quack/quompiler/target/debug/deps/librust_assembler-02d2140120425918.rmeta: assembler/rust_assembler/src/main.rs
-
-assembler/rust_assembler/src/main.rs:
