d8673211cf4b72ae5738eddad0c4a571f3b86ffb
Kozlowski, Alexandre (Stud. DACS) <a.kozlowski@student.maastrichtuniversity.nl>
2026-07-21 12:49:46 +0200

    added intermediary to asm pass

--- compiler/rust_compiler/src/icbm_asm.rs
+++ compiler/rust_compiler/src/icbm_asm.rs
@@ -1,4 +1,4 @@
-use crate::{icbm, lexer::Tokens::Identifier};
+use crate::{icbm};
 
 #[derive(Clone)]
 pub enum Instructions {
@@ -40,13 +40,93 @@ pub struct Program {
 }
 
 pub fn parse_program(program: icbm::Program) -> Program {
-    parse_function(program.function);
+    let func = parse_function(program.function);
+    Program { function: func }
 }
 
 pub fn parse_function(function: icbm::Function) -> Function {
     let identifier = function.identifier;
 
     let instructions = parse_instructions(function.body);
+
+    Function{identifier, body:instructions}
 }
 
-pub fn parse_instructions(inst: Vec<icbm::Instructions>) {}
+pub fn parse_instructions(inst: Vec<icbm::Instructions>) -> Vec<Instructions> {
+    let mut instructions: Vec<Instructions> = Vec::new();
+
+    for icbm_inst in inst.iter() {
+        match icbm_inst {
+            icbm::Instructions::Return(val) => {
+                match val.clone() {
+                    icbm::Val::Constant(const_val) => {
+                        instructions.push(Instructions::Mov(Operand::Imm(const_val), Operand::Reg(Reg::r0)));
+                    }
+                    icbm::Val::Var(var_val) => {
+                        instructions.push(Instructions::Mov(Operand::Pseudo(var_val), Operand::Reg(Reg::r0)));
+                    }
+                }
+                instructions.push(Instructions::Ret);
+            }
+
+            icbm::Instructions::Unary(operator, src, dst) => {
+                match src.clone() {
+                    icbm::Val::Constant(const_val) => {
+                        match dst {
+                            icbm::Val::Var(var_val) => {
+                                instructions.push(Instructions::Mov(Operand::Imm(const_val), Operand::Pseudo(var_val.clone())));
+                            }
+                            _ => {}
+                        }
+
+                    }
+                    icbm::Val::Var(var_val) => {
+                         match dst {
+                            icbm::Val::Var(var_dest_val) => {
+                                instructions.push(Instructions::Mov(Operand::Pseudo(var_val), Operand::Pseudo(var_dest_val.clone())));
+                            }
+                            _ => {}
+                        }
+
+                    }
+                };
+                match operator {
+                    icbm::Unary_Operator::Negate=>{
+
+                        match dst {
+                            icbm::Val::Var(var_val) => {
+                                instructions.push(Instructions::Unary(Unary_Operator::Neg,Operand::Pseudo(var_val.clone())));
+                            },
+                            _=>{}
+                        }
+
+
+                    },
+                    icbm::Unary_Operator::Complement=>{
+                        match dst {
+                            icbm::Val::Var(var_val) => {
+                                instructions.push(Instructions::Unary(Unary_Operator::Not,Operand::Pseudo(var_val.clone())));
+                            },
+                            _=>{}
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
+    return instructions;
+}
+
+
+pub fn convert_op(op: icbm::Unary_Operator) -> Unary_Operator {
+    return match op {
+        icbm::Unary_Operator::Complement => {
+            Unary_Operator::Not
+        }
+        icbm::Unary_Operator::Negate => {
+            Unary_Operator::Neg
+        }
+    };
+}
